81fd51ad48876c249e484b819cfa91e678e85ae5
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-02 14:36:11 +0100

    changes to ZStack

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {mainColor.ignoresSafeArea()
+            VStack{
+                Text("1/10 Questions")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the frist computer Bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                
+            }
+        }.foregroundColor(.white)
+            .padding()
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
