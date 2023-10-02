d7faf314bec3c6a8eac93a787bbef4497b8021bd
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-02 01:10:51 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by user on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
