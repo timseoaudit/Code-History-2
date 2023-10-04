f47164d36a1d9f8d401961ebc0527b53e42a99c2
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-04 02:42:14 +0100

    adjust Hirachy. Add Game logic

---
+++ Code History/Models/Game.swift
@@ -0,0 +1,50 @@
+//
+//  Game.swift
+//  Code History
+//
+//  Created by user on 04/10/2023.
+//
+
+import Foundation
+
+struct Game {
+    //privite declarations
+    private(set) var guesses = [Question: Int]()
+    private let questions = Question.allQuestions.shuffled()
+    private(set) var currentQuestionIndex = 0
+    private(set) var isOver = false
+    
+    //Internal declarations
+    var numberOfQuestions: Int {
+        questions.count
+    }
+    
+    var currentQuestion: Question {
+        questions[currentQuestionIndex]
+    }
+    
+    var guessCount: (correct: Int, incorrect:Int){
+        var count: (correct: Int, incorrect: Int) = (0, 0)
+        for (question, guessedIndex) in guesses {
+            if question.correctAnswerIndex == guessedIndex {
+                count.correct += 1
+            }else{
+                count.incorrect += 1
+            }
+        }
+        return count
+    }
+    
+    //Methods
+    mutating func makeGuessForCurrentQuestion(atIndex index: Int){
+        guesses[currentQuestion] = index
+    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1{
+            currentQuestionIndex += 1
+        }else {
+            isOver = true
+        }
+    }
+}
---
+++ Code History/Models/Question.swift
@@ -0,0 +1,40 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by user on 03/10/2023.
+//
+
+import Foundation
+
+struct Question: Hashable {
+    //lets
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    //property
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"],
+                 correctAnswerIndex: 0),
+        Question(questionText: "What was the first computer bug?",
+                 possibleAnswers: [
+                    "Ant",
+                    "Beetle",
+                    "Moth",
+                    "Fly"],
+                 correctAnswerIndex: 2)
+    ]
+}
