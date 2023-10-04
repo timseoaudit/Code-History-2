6804f2321bf69114584b4f9194791ddca7bbc3c4
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-04 02:05:54 +0100

    create Game.swift and create Models folder

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     correctAnswerIndex: 2)
     
    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     
     var body: some View {
         
---
+++ Models/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  Code History
+//
+//  Created by user on 04/10/2023.
+//
+
+import Foundation
+
+struct Game {
+    
+}
