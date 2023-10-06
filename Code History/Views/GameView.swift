44cc061eae28f6bb318e43a2adfd865707655ccb
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-06 03:27:45 +0100

    refactor code

--- Code History/Views/GameView.swift
+++ Code History/Views/GameView.swift
@@ -9,43 +9,22 @@ import SwiftUI
 
 struct GameView: View {
     
-    let question = Question(
-    questionText: "What was the first computer bug?",
-    possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
-    correctAnswerIndex: 2)
-    
-    @State var mainColor = GameColor.main
+    @StateObject var viewModel = GameViewModel()
 
     
     var body: some View {
-        
         ZStack{
-            mainColor.ignoresSafeArea() //why does it not work ? should Cover whole Display.
+            GameColor.main.ignoresSafeArea() //why does it not work ? should Cover whole Display.
             VStack{
-                Text("1/10")
+                Text(viewModel.questionProgessText)
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
-                Text(question.questionText)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.leading)
-                Spacer()
-                HStack{
-                    ForEach(0 ..< question.possibleAnswers.count) {
-                        answerIndex in
-                        Button(action: {
-                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? GameColor.correctGuess : GameColor.incorrectGuess
-                          }, label: {
-                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-                          })
-
-                    }
-                }
-                
+                QuestionView(question: viewModel.currentQuestion)
+  
             }
-        }.foregroundColor(.white)
+        }
+        .foregroundColor(.white)
             .padding()
         }
     }
